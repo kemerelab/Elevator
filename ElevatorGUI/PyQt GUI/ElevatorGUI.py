a7648afa35d2f52434ac941a7320f3482a8bd9dd
austinay96 <austinay96@gmail.com>
2016-05-25 15:49:39 -0500

    Update ElevatorGUI.py
    Force the motor to ignore invalid inputs.
    Set upper limits on speed and distance.
    Integrate retranslateUi into setupUi as variables can be named when they are created.
    Simplify sendMotorData for more flexibility.
    Remove class scope from variables that do not require it.
    Fix hyperlink.

--- ElevatorGUI/PyQt GUI/ElevatorGUI.py
+++ ElevatorGUI/PyQt GUI/ElevatorGUI.py
@@ -7,229 +7,150 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
 import sys
+import RNELBanner_rc
+from PyQt4 import QtCore, QtGui
 from serial import *
 
-arduino = Serial('/dev/ttyACM1', 115200)
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+arduino = Serial('/dev/cu.usbmodem1411', 115200)
 
 class Ui_Form(QtGui.QWidget):
     def __init__(self):
-        QtGui.QWidget.__init__(self)
-        self.setupUi(self)
+        super(Ui_Form, self).__init__()
+        self.setupUi()
 
-    def setupUi(self, Form):
-        Form.setObjectName(_fromUtf8("Form"))
-        Form.resize(285, 455)
-        Form.setMouseTracking(False)
-        Form.setFocusPolicy(QtCore.Qt.NoFocus)
-        self.formLayout_2 = QtGui.QFormLayout(Form)
-        self.formLayout_2.setFieldGrowthPolicy(QtGui.QFormLayout.AllNonFixedFieldsGrow)
-        self.formLayout_2.setObjectName(_fromUtf8("formLayout_2"))
-        self.frame = QtGui.QFrame(Form)
-        self.frame.setFrameShape(QtGui.QFrame.Panel)
-        self.frame.setFrameShadow(QtGui.QFrame.Raised)
-        self.frame.setObjectName(_fromUtf8("frame"))
-        self.gridLayout = QtGui.QGridLayout(self.frame)
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.btn_run = QtGui.QPushButton(self.frame)
-        self.btn_run.setMaximumSize(QtCore.QSize(162, 27))
-        self.btn_run.setObjectName(_fromUtf8("btn_run"))
-        self.gridLayout.addWidget(self.btn_run, 3, 0, 1, 1, QtCore.Qt.AlignHCenter)
-        self.verticalLayout_5 = QtGui.QVBoxLayout()
-        self.verticalLayout_5.setObjectName(_fromUtf8("verticalLayout_5"))
-        self.label_instructions = QtGui.QLabel(self.frame)
+    def setupUi(self):
+        self.resize(285, 455)
+        self.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.setWindowTitle("RNEL Elevator Controller")
+
+        frame = QtGui.QFrame(self)
+        formLayout_2 = QtGui.QFormLayout(self)
+        formLayout_2.addRow(frame)
+
+        btn_run = QtGui.QPushButton("Run", frame)
+        btn_run.setMaximumSize(QtCore.QSize(162, 27))
+
         font = QtGui.QFont()
-        font.setPointSize(8)
-        self.label_instructions.setFont(font)
-        self.label_instructions.setObjectName(_fromUtf8("label_instructions"))
-        self.verticalLayout_5.addWidget(self.label_instructions)
-        self.label_website = QtGui.QLabel(self.frame)
-        font = QtGui.QFont()
-        font.setPointSize(8)
-        self.label_website.setFont(font)
-        self.label_website.setMouseTracking(True)
-        self.label_website.setOpenExternalLinks(True)
-        self.label_website.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse)
-        self.label_website.setObjectName(_fromUtf8("label_website"))
-        self.verticalLayout_5.addWidget(self.label_website)
-        self.gridLayout.addLayout(self.verticalLayout_5, 5, 0, 1, 1)
+        font.setPointSize(11)
+        label_instructions = QtGui.QLabel("Please visit the following site for instructions:", frame)
+        label_instructions.setFont(font)
+
+        label_website = QtGui.QLabel(frame)
+        label_website.setFont(font)
+        label_website.setText("<a href=\"http://nel.rice.edu/\">Elevator Maze</a>")
+        label_website.setTextInteractionFlags(QtCore.Qt.LinksAccessibleByMouse)
+        label_website.setOpenExternalLinks(True)
+
+        verticalLayout_5 = QtGui.QVBoxLayout()
+        verticalLayout_5.addWidget(label_instructions)
+        verticalLayout_5.addWidget(label_website)
+
+        label_banner = QtGui.QLabel(frame)
+        label_banner.setMaximumSize(QtCore.QSize(219, 102))
+        label_banner.setText("")
+        label_banner.setPixmap(QtGui.QPixmap(":/RNELicon/RNELBanner.png"))
+        label_banner.setScaledContents(True)
+
+        widget = QtGui.QWidget(frame)
         spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem, 2, 0, 1, 1)
-        spacerItem1 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        self.gridLayout.addItem(spacerItem1, 4, 0, 1, 1)
-        self.label_banner = QtGui.QLabel(self.frame)
-        self.label_banner.setMaximumSize(QtCore.QSize(219, 102))
-        self.label_banner.setText(_fromUtf8(""))
-        self.label_banner.setPixmap(QtGui.QPixmap(_fromUtf8(":/RNELicon/RNELBanner.png")))
-        self.label_banner.setScaledContents(True)
-        self.label_banner.setObjectName(_fromUtf8("label_banner"))
-        self.gridLayout.addWidget(self.label_banner, 0, 0, 1, 1)
-        self.widget = QtGui.QWidget(self.frame)
-        self.widget.setObjectName(_fromUtf8("widget"))
-        self.formLayout_4 = QtGui.QFormLayout(self.widget)
-        self.formLayout_4.setFieldGrowthPolicy(QtGui.QFormLayout.AllNonFixedFieldsGrow)
-        self.formLayout_4.setMargin(0)
-        self.formLayout_4.setObjectName(_fromUtf8("formLayout_4"))
-        spacerItem2 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        self.formLayout_4.setItem(1, QtGui.QFormLayout.LabelRole, spacerItem2)
-        self.label_motorState = QtGui.QLabel(self.widget)
-        font = QtGui.QFont()
+
         font.setPointSize(12)
         font.setBold(True)
         font.setUnderline(False)
         font.setWeight(75)
-        self.label_motorState.setFont(font)
-        self.label_motorState.setObjectName(_fromUtf8("label_motorState"))
-        self.formLayout_4.setWidget(2, QtGui.QFormLayout.SpanningRole, self.label_motorState)
-        self.horizontalLayout = QtGui.QHBoxLayout()
-        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.verticalLayout = QtGui.QVBoxLayout()
-        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        self.label_speed = QtGui.QLabel(self.widget)
-        self.label_speed.setObjectName(_fromUtf8("label_speed"))
-        self.verticalLayout.addWidget(self.label_speed)
-        self.label_steps = QtGui.QLabel(self.widget)
-        self.label_steps.setObjectName(_fromUtf8("label_steps"))
-        self.verticalLayout.addWidget(self.label_steps)
-        self.label_direction = QtGui.QLabel(self.widget)
-        self.label_direction.setObjectName(_fromUtf8("label_direction"))
-        self.verticalLayout.addWidget(self.label_direction)
-        self.label_mode = QtGui.QLabel(self.widget)
-        self.label_mode.setObjectName(_fromUtf8("label_mode"))
-        self.verticalLayout.addWidget(self.label_mode)
-        self.horizontalLayout.addLayout(self.verticalLayout)
-        self.formLayout_3 = QtGui.QFormLayout()
-        self.formLayout_3.setObjectName(_fromUtf8("formLayout_3"))
-        self.lineEdit_speed = QtGui.QLineEdit(self.widget)
+        label_motorState = QtGui.QLabel("Stepper Motor Parameters", widget)
+        label_motorState.setFont(font)
+
+        formLayout_4 = QtGui.QFormLayout(widget)
+        formLayout_4.setFieldGrowthPolicy(QtGui.QFormLayout.AllNonFixedFieldsGrow)
+        formLayout_4.setMargin(0)
+        formLayout_4.addItem(spacerItem)
+        formLayout_4.addRow(label_motorState)
+
+        label_speed = QtGui.QLabel("Speed:", widget)
+        label_steps = QtGui.QLabel("Distance:", widget)
+        label_direction = QtGui.QLabel("Direction:", widget)
+        label_mode = QtGui.QLabel("Mode:", widget)
+
+        verticalLayout = QtGui.QVBoxLayout()
+        verticalLayout.addWidget(label_speed)
+        verticalLayout.addWidget(label_steps)
+        verticalLayout.addWidget(label_direction)
+        verticalLayout.addWidget(label_mode)
+
+        horizontalLayout = QtGui.QHBoxLayout()
+        horizontalLayout.addLayout(verticalLayout)
+        formLayout_4.setLayout(3, QtGui.QFormLayout.LabelRole, horizontalLayout)
+
+        self.lineEdit_speed = QtGui.QLineEdit(widget)
         self.lineEdit_speed.setMaximumSize(QtCore.QSize(111, 27))
-        self.lineEdit_speed.setPlaceholderText(_fromUtf8(""))
-        self.lineEdit_speed.setObjectName(_fromUtf8("lineEdit_speed"))
-        self.formLayout_3.setWidget(0, QtGui.QFormLayout.LabelRole, self.lineEdit_speed)
-        self.lineEdit_distance = QtGui.QLineEdit(self.widget)
+        self.lineEdit_speed.setText("0")
+        self.lineEdit_distance = QtGui.QLineEdit(widget)
         self.lineEdit_distance.setMaximumSize(QtCore.QSize(111, 27))
-        self.lineEdit_distance.setObjectName(_fromUtf8("lineEdit_distance"))
-        self.formLayout_3.setWidget(1, QtGui.QFormLayout.LabelRole, self.lineEdit_distance)
-        self.comboBox_direction = QtGui.QComboBox(self.widget)
+        self.lineEdit_distance.setText("0")
+        self.comboBox_direction = QtGui.QComboBox(widget)
         self.comboBox_direction.setMaximumSize(QtCore.QSize(111, 27))
-        self.comboBox_direction.setObjectName(_fromUtf8("comboBox_direction"))
-        self.comboBox_direction.addItem(_fromUtf8(""))
-        self.comboBox_direction.addItem(_fromUtf8(""))
-        self.formLayout_3.setWidget(3, QtGui.QFormLayout.SpanningRole, self.comboBox_direction)
-        self.comboBox_mode = QtGui.QComboBox(self.widget)
+        self.comboBox_direction.addItems(["Up", "Down"])
+        self.comboBox_mode = QtGui.QComboBox(widget)
         self.comboBox_mode.setMaximumSize(QtCore.QSize(111, 27))
-        self.comboBox_mode.setObjectName(_fromUtf8("comboBox_mode"))
-        self.comboBox_mode.addItem(_fromUtf8(""))
-        self.comboBox_mode.addItem(_fromUtf8(""))
-        self.comboBox_mode.addItem(_fromUtf8(""))
-        self.comboBox_mode.addItem(_fromUtf8(""))
-        self.formLayout_3.setWidget(4, QtGui.QFormLayout.SpanningRole, self.comboBox_mode)
-        self.label_in = QtGui.QLabel(self.widget)
-        self.label_in.setObjectName(_fromUtf8("label_in"))
-        self.formLayout_3.setWidget(1, QtGui.QFormLayout.FieldRole, self.label_in)
-        self.label_RPM = QtGui.QLabel(self.widget)
-        self.label_RPM.setObjectName(_fromUtf8("label_RPM"))
-        self.formLayout_3.setWidget(0, QtGui.QFormLayout.FieldRole, self.label_RPM)
-        self.horizontalLayout.addLayout(self.formLayout_3)
-        self.formLayout_4.setLayout(3, QtGui.QFormLayout.LabelRole, self.horizontalLayout)
-        self.gridLayout.addWidget(self.widget, 1, 0, 1, 1, QtCore.Qt.AlignHCenter)
-        self.formLayout_2.setWidget(1, QtGui.QFormLayout.SpanningRole, self.frame)
+        self.comboBox_mode.addItems(["Single", "Double", "Interleave", "Microstep"])
+        label_in = QtGui.QLabel("in.", widget)
+        label_RPM = QtGui.QLabel("RPM", widget)
 
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
+        formLayout_3 = QtGui.QFormLayout()
+        formLayout_3.addRow(self.lineEdit_speed, label_RPM)
+        formLayout_3.addRow(self.lineEdit_distance, label_in)
+        formLayout_3.addRow(self.comboBox_direction)
+        formLayout_3.addRow(self.comboBox_mode)
+        horizontalLayout.addLayout(formLayout_3)
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(_translate("Form", "RNEL Elevator Controller", None))
-        self.btn_run.setText(_translate("Form", "Run", None))
-        self.label_instructions.setText(_translate("Form", "Please visit the following site for instructions :", None))
-        self.label_website.setText(_translate("Form", "http://www.fakewebsite.com", None))
-        self.label_motorState.setText(_translate("Form", "    Stepper Motor Parameters", None))
-        self.label_speed.setText(_translate("Form", "Speed:", None))
-        self.label_steps.setText(_translate("Form", "Distance:", None))
-        self.label_direction.setText(_translate("Form", "Direction:", None))
-        self.label_mode.setText(_translate("Form", "Mode:", None))
-        self.lineEdit_speed.setText(_translate("Form", "100", None))
-        self.lineEdit_distance.setText(_translate("Form", "12", None))
-        self.comboBox_direction.setItemText(0, _translate("Form", "Up", None))
-        self.comboBox_direction.setItemText(1, _translate("Form", "Down", None))
-        self.comboBox_mode.setItemText(0, _translate("Form", "Double", None))
-        self.comboBox_mode.setItemText(1, _translate("Form", "Single", None))
-        self.comboBox_mode.setItemText(2, _translate("Form", "Microstep", None))
-        self.comboBox_mode.setItemText(3, _translate("Form", "Interleave", None))
-        self.label_in.setText(_translate("Form", "in.", None))
-        self.label_RPM.setText(_translate("Form", "RPM", None))
-        self.btn_run.clicked.connect(self.sendMotorData)
+        gridLayout = QtGui.QGridLayout(frame)
+        gridLayout.addWidget(label_banner, 0, 0, 1, 1)
+        gridLayout.addWidget(widget, 1, 0, 1, 1, QtCore.Qt.AlignHCenter)
+        gridLayout.addItem(spacerItem, 2, 0, 1, 1)
+        gridLayout.addWidget(btn_run, 3, 0, 1, 1, QtCore.Qt.AlignHCenter)
+        gridLayout.addItem(spacerItem, 4, 0, 1, 1)
+        gridLayout.addLayout(verticalLayout_5, 5, 0, 1, 1)
+
+        btn_run.clicked.connect(self.sendMotorData)
 
     def sendMotorData(self):
-        speed_string = "0123"
-        steps_string = "012345"
-        direction_string = ""
-        mode_string = "Z"
-        data = ""
-
-        speed = self.lineEdit_speed.text()
-        distance = self.lineEdit_distance.text()
+        speed, speed_valid = QtCore.QString.toFloat(self.lineEdit_speed.text())
+        distance, distance_valid = QtCore.QString.toFloat(self.lineEdit_distance.text())
         direction = self.comboBox_direction.currentText()
         mode = self.comboBox_mode.currentText()[0]
 
-        steps = float(distance) * 646.806393
-        steps = str(int(round(steps)))
-        print steps
-        print len(steps)
+        # RPMs of higher than 43 will result in skipped steps; set upper limit
+        if speed > 43:
+            speed = 0
 
-        if len(speed) == 1:
-            speed_string = speed_string.replace('3', speed, 1)
-            speed_string = speed_string.replace('12', '00', 1)
-        elif len(speed) == 2:
-            speed_string = speed_string.replace('23', speed, 1)
-            speed_string = speed_string.replace('1', '0', 1)
-        elif len(speed) == 3:
-            speed_string = speed_string.replace('123', speed, 1)
-        elif len(speed) == 4:
-            speed_string = speed_string.replace('0123', speed, 1)
+        speed = str(int(speed))
+        while len(speed) < 4:
+            speed = "0" + speed
 
-        if len(steps) == 1:
-            steps_string = steps_string.replace('5', steps, 1)
-            steps_string = steps_string.replace('01234', '00000', 1)
-        elif len(steps) == 2:
-            steps_string = steps_string.replace('45', steps, 1)
-            steps_string = steps_string.replace('0123', '0000', 1)
-        elif len(steps) == 3:
-            steps_string = steps_string.replace('345', steps, 1)
-            steps_string = steps_string.replace('012', '000', 1)
-        elif len(steps) == 4:
-            steps_string = steps_string.replace('2345', steps, 1)
-            steps_string = steps_string.replace('01', '00', 1)
-        elif len(steps) == 5:
-            steps_string = steps_string.replace('12345', steps, 1)
-        elif len(steps) == 6:
-            steps_string = steps_string.replace('012345', steps, 1)
+        # set upper limit depending on height of maze
+        if distance > 200:
+            distance = 0
 
-        mode_string = mode_string.replace('Z', mode[0], 1)
+        # steps_perInch depends on pulley diameter; using NEMA23 drive shaft diameter (6.35mm) for now
+        steps_perInch = 254.647771437
+        steps = distance * steps_perInch
+        steps = str(int(steps))
+        while len(steps) < 6:
+            steps = "0" + steps
 
-        data = 'x'+speed_string+'x'+steps_string+'x'+mode_string+'x'+direction
+        data = 'x'+speed+'x'+steps+'x'+mode+'x'+direction
 
         arduino.write(str(data)) 
         print str(data)       
 
-import RNELBanner_rc
-
+# following code is not executed if this module is imported
 if __name__ == '__main__':
+    # calls constructor of QApplication to initialize QT application
     app = QtGui.QApplication(sys.argv)
+
     ex = Ui_Form()
     ex.show()
+    ex.raise_()
     sys.exit(app.exec_())
